1e5cdadc32c42c4c64f122564dbc6e77755e9342
Atkrista <atulaniya1@gmail.com>
2022-08-29 21:20:04 +0545

    Completed Tokenizer.

--- Compiler/JackAnalyzer.py
+++ Compiler/JackAnalyzer.py
@@ -1,58 +1,60 @@
 import sys
 import os
-from Compiler.constants import TokenType
+from constants import TokenType
 from JackTokenizer import JackTokenizer
 from CompilationEngine import CompilationEngine
 
 
 class JackAnalyzer:
-    def __init__(self, input) -> None:
-        self.input = input
+    def __init__(self) -> None:
+        pass
 
     def out_file(self, infile):
         """Name of the xml file to write into."""
-        return infile.split[:5] + "T.xml"
+        print(infile[:-5] + "T.xml")
+        return infile[:-5] + "T.xml"
 
-    def is_jack_file(self):
-        """Checks if `name` is a jack file i.e file_name.jack ."""
-        return self.input.split(".")[1] == "jack"
+    def is_jack_file(self, input):
+        """Checks if `name` is a jack file i.e `file_name.jack` ."""
+        return os.path.splitext(input)[1] == ".jack"
 
-    def compile_file(self) -> None:
-        with open(self.input, "r") as infile, open(
-            self.out_file(self.input), "w"
+    def compile_file(self, file_name) -> None:
+        with open(file_name, "r") as infile, open(
+            self.out_file(file_name), "w"
         ) as outfile:
             tokenizer = JackTokenizer(infile)
             token = ""
             tag = ""
             while tokenizer.has_more_tokens():
-                if tokenizer.token_type == TokenType.KEYWORD:
+                if tokenizer.token_type() == TokenType.KEYWORD:
                     token = tokenizer.key_word()
                     tag = TokenType.KEYWORD.value
-                elif tokenizer.token_type == TokenType.SYMBOL:
+                elif tokenizer.token_type() == TokenType.SYMBOL:
                     token = tokenizer.symbol()
                     tag = TokenType.SYMBOL.value
-                elif tokenizer.token_type == TokenType.IDENTIFIER:
+                elif tokenizer.token_type() == TokenType.IDENTIFIER:
                     token = tokenizer.identifier()
                     tag = TokenType.IDENTIFIER.value
-                elif tokenizer.token_type == TokenType.INT_CONST:
+                elif tokenizer.token_type() == TokenType.INT_CONST:
                     token = tokenizer.int_val()
                     tag = TokenType.INT_CONST.value
-                elif tokenizer.token_type == TokenType.STRING_CONST:
+                elif tokenizer.token_type() == TokenType.STRING_CONST:
                     token = tokenizer.string_val()
                     tag = TokenType.STRING_CONST.value
-                outfile.write(f"<{tag}>{token}</{tag}>")
+                outfile.write(f"<{tag}>{token}</{tag}>\n")
                 tokenizer.advance()
 
-    def compile_dir(self) -> None:
-        pass
+    def compile_dir(self, dir_name) -> None:
+        for file_name in os.listdir(dir_name):
+            self.compile_file(file_name)
 
-    def compile(self) -> None:
-        if self.is_jack_file():
-            self.compile_file()
+    def compile(self, input) -> None:
+        if self.is_jack_file(input):
+            self.compile_file(input)
         else:
-            self.compile_dir()
+            self.compile_dir(input)
 
 
 if __name__ == "__main__":
     arg1 = sys.argv[1]
-    JackAnalyzer(arg1).compile()
+    JackAnalyzer().compile(arg1)
--- Compiler/JackTokenizer.py
+++ Compiler/JackTokenizer.py
@@ -1,58 +1,88 @@
+from typing import NamedTuple
 from constants import TokenType, KeyWord, Symbols, token_keyword_map
 import re
+
+
+class Token(NamedTuple):
+    token_type: TokenType
+    value: str
 
 
 class JackTokenizer:
     def __init__(self, input) -> None:
         """Opens the input .jack file/stream and gets ready to tokeninze it."""
-        self.tokens = []
-        self.current_token = ""
-        for line in input.readlines():
-            self.tokens.append(line.split())
+        self._tokens = []
+        self._tokenize(input)
+        self.current_token = self._tokens.pop(0)
 
     def has_more_tokens(self) -> bool:
         """Are there any more tokens in the input?"""
-        return bool(self.tokens)
+        return bool(self._tokens)
 
     def advance(self) -> None:
         """Gets the next token from the input and makes it the current token.
         This method should be called only if has_more_tokens returns True.
         Initially there is no current token."""
-        self.current_token = self.tokens.pop(0)
+        self.current_token = self._tokens.pop(0)
 
     def token_type(self) -> TokenType:
         """Returns the type of the current token."""
-        if self.current_token in (e.value for e in KeyWord):
-            return TokenType.KEYWORD
-        elif self.current_token in Symbols:
-            return TokenType.SYMBOL
-        elif re.match(r"^\d+$", self.current_token):
-            return TokenType.INT_CONST
-        elif re.match(r"^[a-zA-Z]+$", self.current_token):
-            return TokenType.STRING_CONST
-        elif re.match(r"^\D\w+$", self.current_token):
-            return TokenType.IDENTIFIER
+        return self.current_token.token_type
 
     def key_word(self) -> KeyWord:
         """Returns the keyword which is the current token as a constant."""
-        return token_keyword_map[self.current_token]
+        # return self.current_token.value
+        return token_keyword_map[self.current_token.value].value
 
     def symbol(self) -> str:
         """Returns the character which is the current token. Should only be
         called if token_type is SYMBOL."""
-        return self.current_token
+        return self.current_token.value
 
     def identifier(self) -> str:
         """Returns the string which is the current token. Should only be
         called if token_type is IDENTIFIER."""
-        return self.current_token
+        return self.current_token.value
 
     def int_val(self) -> int:
         """Returns the integer value of the current token. Should only be
         called if token_type is INT_CONST."""
-        return int(self.current_token)
+        return int(self.current_token.value)
 
     def string_val(self) -> str:
         """Returns the string value of the current token without the opening and
         closing double quotes. Should only be called if token_type is STRING_CONST."""
-        return self.current_token[1:-1]
+        return self.current_token.value[1:-1]
+
+    def _tokenize(self, input):
+        token_specification = [
+            ("SKIP", r"\s+"),
+            ("KEYWORD", rf"{'|'.join((e.value for e in KeyWord))}"),
+            ("SYMBOLS", r"{|}|\(|\)|[|]|\.|,|;|\-|\+|\*|/|&|\||<|>|=|~"),
+            ("INT_CONST", r"\d+"),
+            ("STRING_CONST", r'".*"'),
+            ("IDENTIFIER", r"\w+"),
+        ]
+        tok_regex = "|".join("(?P<%s>%s)" % pair for pair in token_specification)
+        # print(tok_regex)
+        temp = input.read()
+        # Remove comments
+        temp = re.sub(r"//.*\n", "", temp)
+        temp = re.sub(r"/\*.*\*/", "", temp)
+        for mo in re.finditer(tok_regex, temp):
+            kind = mo.lastgroup
+            value = mo.group()
+            if kind == "SKIP":
+                continue
+            if value:
+                if kind == "KEYWORD":
+                    token_type = TokenType.KEYWORD
+                elif kind == "SYMBOLS":
+                    token_type = TokenType.SYMBOL
+                elif kind == "INT_CONST":
+                    token_type = TokenType.INT_CONST
+                elif kind == "IDENTIFIER":
+                    token_type = TokenType.IDENTIFIER
+                else:
+                    token_type = TokenType.STRING_CONST
+                self._tokens.append(Token(token_type, value))
